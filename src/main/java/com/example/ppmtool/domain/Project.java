3e72456052bdffb5ff5ba1d21c74a43454a36ea1
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-18 12:49:02 -0700

    Project database model. Initial project repository. H2 database setup.

---
+++ src/main/java/com/example/ppmtool/domain/Project.java
@@ -0,0 +1,64 @@
+package com.example.ppmtool.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Project {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    public Project() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+}
