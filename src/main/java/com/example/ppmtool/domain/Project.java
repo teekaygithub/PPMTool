2a66f46964cf91599c8eb553cc8c6b190caada80
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-18 17:13:24 -0700

    Restricted project ID to be unique. Moved error mapping to its own service.

--- src/main/java/com/example/ppmtool/domain/Project.java
+++ src/main/java/com/example/ppmtool/domain/Project.java
@@ -16,6 +16,7 @@ public class Project {
     private String projectName;
     @NotBlank(message="Project identifier is required")
     @Size(min=4, max=5, message="Please use 4 to 5 characters for the project identifier")
+    @Column(updatable=false, unique=true)
     private String projectIdentifier;
     @NotBlank(message="Project description is required")
     private String description;
--- src/main/java/com/example/ppmtool/web/ProjectController.java
+++ src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -2,16 +2,21 @@ package com.example.ppmtool.web;
 
 import com.example.ppmtool.domain.Project;
 import com.example.ppmtool.services.ProjectService;
+import com.example.ppmtool.services.MapValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,11 +25,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
-        }
+        
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap!=null) {return errorMap;}
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
