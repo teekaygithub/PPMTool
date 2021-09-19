2021f83c46f106635a7d1cf100801c5bfd88d30e
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-18 17:45:59 -0700

    Exception handling to enforce uniqueness of project IDs.

--- src/main/java/com/example/ppmtool/services/ProjectService.java
+++ src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -2,6 +2,7 @@ package com.example.ppmtool.services;
 
 import com.example.ppmtool.repositories.ProjectRepository;
 import com.example.ppmtool.domain.Project;
+import com.example.ppmtool.exceptions.ProjectIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
