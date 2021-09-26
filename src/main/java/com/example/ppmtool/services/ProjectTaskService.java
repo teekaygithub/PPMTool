9855204d708a0b17a9d5b108447316de95be35f7
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-25 21:06:10 -0700

    Switched to MySQL database. Added exception handling for nonexistent projects when sending GET/POST requests for project tasks.

--- src/main/java/com/example/ppmtool/services/ProjectTaskService.java
+++ src/main/java/com/example/ppmtool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package com.example.ppmtool.services;
 
 import com.example.ppmtool.domain.Backlog;
+import com.example.ppmtool.domain.Project;
 import com.example.ppmtool.domain.ProjectTask;
+import com.example.ppmtool.exceptions.ProjectNotFoundException;
 import com.example.ppmtool.repositories.BacklogRepository;
+import com.example.ppmtool.repositories.ProjectRepository;
 import com.example.ppmtool.repositories.ProjectTaskRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,29 +20,40 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        projectTask.setBacklog(backlog);
-        Integer backlogSequence = backlog.getPTSequence();
-        backlogSequence++;
-        backlog.setPTSequence(backlogSequence);
-        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
-        
-        // Initial status when priority null
-        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
-            projectTask.setPriority(3);
-        } 
+        try {
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
+            Integer backlogSequence = backlog.getPTSequence();
+            backlogSequence++;
+            backlog.setPTSequence(backlogSequence);
+            projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
+            
+            // Initial status when priority null
+            if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+                projectTask.setPriority(3);
+            } 
 
-        // Initial status when status null
-        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-            projectTask.setStatus("TO_DO");
+            // Initial status when status null
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project not found");
         }
-
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBacklogById(String id) {
+        Project project = projectRepository.findByProjectIdentifier(id);
+        if (project == null) {
+            throw new ProjectNotFoundException("Project with ID '" + id + "' not found");
+        }
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
