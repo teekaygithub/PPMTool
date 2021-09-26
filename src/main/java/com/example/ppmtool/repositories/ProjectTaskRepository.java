171f3801545cd36b11130f7b65b33cd227d5e5f2
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-25 20:21:04 -0700

    GET route for retrieving all project tasks by project ID

--- src/main/java/com/example/ppmtool/repositories/ProjectTaskRepository.java
+++ src/main/java/com/example/ppmtool/repositories/ProjectTaskRepository.java
@@ -1,5 +1,7 @@
 package com.example.ppmtool.repositories;
 
+import java.util.List;
+
 import com.example.ppmtool.domain.ProjectTask;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
-    
+    public List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);    
 }
--- src/main/java/com/example/ppmtool/web/BacklogController.java
+++ src/main/java/com/example/ppmtool/web/BacklogController.java
@@ -9,11 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/backlog")
@@ -36,5 +38,10 @@ public class BacklogController {
         ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
 
         return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{backlog_id}")
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id) {
+        return projectTaskService.findBacklogById(backlog_id);
     }
 }
